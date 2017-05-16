4dd6561da312d8d4e6b79090c0e44a80aac4ab4b
Alessandro Rubio <rubmoa@gmail.com>
2017-05-16 20:45:17 +0200

    Needs more configuration, I'll a branch for that

--- img/views.py
+++ img/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import ImageForm
+from PIL import Image, ImageOps
+import stripe
 
 # Create your views here.
+def egami(request):
+    #TODO: Mirror image and then redirect download after paayement process
+    pass
