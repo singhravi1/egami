b1f4eac226d31e59fa19c169c8e4026f3cd96e1b
Alessandro Rubio <rubmoa@gmail.com>
2017-05-16 20:44:27 +0200

    One image model with Pillow parameters

--- img/models.py
+++ img/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Image_client(models.Model):
+    """
+    Ce modele contient un seul element principal: Image.
+    Les deux autres sont automatiquement remplis par Pillow
+    """
+    image = models.ImageField(null=False, blank=False, height_field='height_field', width_field='width_field')
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.image
