e7f254ddba72ec4193a01efda1e11bec927e507d
Alessandro Rubio <rubmoa@gmail.com>
2017-05-16 20:43:23 +0200

    Added new model url and basic media routing

--- egami/urls.py
+++ egami/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from img import views as img_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', img_views.egami, name='egami')
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
