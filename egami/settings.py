3d27ffcd239c699f4c4f7a6ff82eb9f7692b1d53
Alessandro Rubio <rubmoa@gmail.com>
2017-05-17 22:55:03 +0200

    Config for img

--- egami/settings.py
+++ egami/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'img.apps.ImgConfig',
 ]
 
 MIDDLEWARE = [
@@ -115,9 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join('static_cdn')
-MEDIA_ROOT = os.path.join('media_cdn')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media_cdn')
