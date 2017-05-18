216fcc0f11bc0da6e185ed6d6c4dfa47625f28ef
Alessandro Rubio <rubmoa@gmail.com>
2017-05-18 19:51:12 +0200

    Changed slug to id

--- img/urls.py
+++ img/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 
 		url(r'^$', views.index, name = 'index'),
 		url(r'^new$', views.new, name='new'),
-		url(r'^(?P<slug>[\w-]+)/$', views.detail, name='detail'),
+		url(r'^(?P<pk>[\w-]+)/$', views.detail, name='detail'),
   ]
